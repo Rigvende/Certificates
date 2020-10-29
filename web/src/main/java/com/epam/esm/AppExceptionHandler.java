87d10f69dd667a5ae4b7e1c5ae0b7c81d465ebde
Marianna Patrusova <sumeragi.tanako@gmail.com>
2020-10-29 14:13:56 +0300

    exception handler refactoring

--- web/src/main/java/com/epam/esm/AppExceptionHandler.java
+++ web/src/main/java/com/epam/esm/AppExceptionHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import static com.epam.esm.error.ErrorMessage.*;
 
@@ -19,10 +18,8 @@ public class AppExceptionHandler {
 
     @ExceptionHandler(value
             = { NoHandlerFoundException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ResponseEntity<Object> handleConflict(NoHandlerFoundException ex) {
-            ex.printStackTrace();
-            return ResponseEntity.ok(new CustomError(40400, ERROR_404));
+    public ResponseEntity<Object> handleConflict() {
+            return new ResponseEntity<>(new CustomError(40400, ERROR_404), HttpStatus.NOT_FOUND);
     }
 
 }
